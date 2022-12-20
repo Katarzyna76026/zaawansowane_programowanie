e488e17fd60b27f3fd0e8fe204a8bdc2cff7ccaf
Katarzyna76026 <katarzyna.warda@edu.uekat.pl>
2022-12-20 08:23:21 +0100

    commit

--- main.py
+++ main.py
@@ -7,37 +7,16 @@ img1 = cv2.imread('C:/Users/Huawei/PycharmProjects/LiczenieLudzi/ZaawansowanePro
 img2 = cv2.imread('C:/Users/Huawei/PycharmProjects/LiczenieLudzi/ZaawansowaneProgramowanie/img2.png')
 img3 = cv2.imread('C:/Users/Huawei/PycharmProjects/LiczenieLudzi/ZaawansowaneProgramowanie/img3.png')
 
-img1_cvtClolor = cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
-img2_cvtClolor = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
-img3_cvtClolor = cv2.cvtColor(img3,cv2.COLOR_BGR2RGB)
+images = [img1, img2, img3]
 
+for img in images:
+    img_cvtColor = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    box, label, count = cvlib.detect_common_objects(img)
+    output = draw_bbox(img,box, label,count)
+    output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
+    plt.figure(figsize = (10,10))
+    plt.axis("off")
+    plt.imshow(output)
+    plt.show()
+    print("Na zdjęciu jest " + str(len(label)) + " ludzi")
 
-box, label, count = cvlib.detect_common_objects(img1)
-output = draw_bbox(img1,box, label,count)
-output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
-plt.figure(figsize = (10,10))
-plt.axis("off")
-plt.imshow(output)
-plt.show()
-
-print("Na zdjęciu jest " + str(len(label)) + " ludzi")
-
-box, label, count = cvlib.detect_common_objects(img2)
-output = draw_bbox(img2,box, label,count)
-output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
-plt.figure(figsize = (10,10))
-plt.axis("off")
-plt.imshow(output)
-plt.show()
-
-print("Na zdjęciu jest " + str(len(label)) + " ludzi")
-
-box, label, count = cvlib.detect_common_objects(img3)
-output = draw_bbox(img3,box, label,count)
-output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
-plt.figure(figsize = (10,10))
-plt.axis("off")
-plt.imshow(output)
-plt.show()
-
-print("Na zdjęciu jest " + str(len(label)) + " ludzi")
